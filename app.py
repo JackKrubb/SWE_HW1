c4b335c24f63b5e162074c8ddae173f0c822a88f
JackKrubb <jacklee.leeco@gmail.com>
2023-01-19 21:04:16 +0700

    Added APIs for getting all stocks from all vending machines, get one stock from one vending machine, get all stocks from one vending machine and adding stocks function

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def one_vending(id):
         return None
 
 # Create new vending machine
-@app.route('/vending/create-vending/', methods=['POST'])
+@app.route('/vending/create-vending', methods=['POST'])
 def create_vending():
     vending_form = request.form
     vending_location = vending_form['vending_location']
@@ -64,7 +64,7 @@ def edit_vending(id):
     return redirect('/vending')
 
 # Delete one vending machine
-@app.route('/vending/delete-vending/<int:id>/')
+@app.route('/vending/delete-vending/<int:id>')
 def delete_vending(id):
     cur = mysql.connection.cursor()
     query_statement = f"DELETE FROM vending_machine WHERE vending_id = {id}"
@@ -97,7 +97,7 @@ def one_product(id):
         return None
 
 # Add new product
-@app.route('/product/add-product/', methods=['POST'])
+@app.route('/product/add-product', methods=['POST'])
 def add_product():
     product_form = request.form
     product_name = product_form['product_name']
@@ -123,13 +123,85 @@ def edit_product(id):
     return redirect('/product')
 
 # Delete one product
-@app.route('/product/delete-product/<int:id>/')
+@app.route('/product/delete-product/<int:id>')
 def delete_product(id):
     cur = mysql.connection.cursor()
     query_statement = f"DELETE FROM product WHERE product_id = {id}"
     cur.execute(query_statement)
     mysql.connection.commit()
     return redirect('/product')
+
+# Return all stocks in all vending machine
+@app.route('/stock')
+def all_stock():
+    cur = mysql.connection.cursor()
+    number_of_rows = cur.execute("SELECT * FROM stocking")
+    if(number_of_rows>0):
+        stock = cur.fetchall()
+        cur.close()
+        return jsonify(stock)
+    else:
+        return None
+
+# Return one stock from one vending
+@app.route('/stock/single-stock', methods=['GET'])
+def one_stock_one_vend():
+    stock_form = request.form
+    vending_id = stock_form['vending_id']
+    product_id = stock_form['product_id']
+
+    cur = mysql.connection.cursor()
+    query_statement = f"SELECT * FROM stocking WHERE product_id = {product_id} AND vending_id = {vending_id}"
+    number_of_rows = cur.execute(query_statement)
+    if(number_of_rows>0):
+        stock = cur.fetchone()
+        cur.close()
+        return jsonify(stock)
+    else:
+        return None
+
+# Return all stock from one vending
+@app.route('/stock/single-vend', methods=['GET'])
+def all_stock_one_vend():
+    stock_form = request.form
+    vending_id = stock_form['vending_id']
+
+    cur = mysql.connection.cursor()
+    query_statement = f"SELECT * FROM stocking WHERE vending_id = {vending_id}"
+    number_of_rows = cur.execute(query_statement)
+    if(number_of_rows>0):
+        stock = cur.fetchall()
+        cur.close()
+        return jsonify(stock)
+    else:
+        return None
+
+# Add stocks
+@app.route('/stock/add-stock', methods=['POST'])
+def add_stock():
+    stock_form = request.form
+    vending_id = stock_form['vending_id']
+    product_id = stock_form['product_id']
+    product_amount = stock_form['product_amount']
+
+    cur = mysql.connection.cursor()
+    query_statement_check = f"SELECT * FROM stocking WHERE product_id={product_id} AND vending_id={vending_id}"
+    duplicate = cur.execute(query_statement_check)
+    if duplicate > 0:
+        stock = cur.fetchone()
+        stocking_id = stock['stocking_id']
+        new_product_amount = int(stock['product_amount']) + int(product_amount)
+        query_statement = f"UPDATE stocking SET product_amount={new_product_amount} WHERE stocking_id={stocking_id}"
+        cur.execute(query_statement)
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/stock')
+    else:
+        query_statement = f"INSERT INTO stocking(vending_id, product_id, product_amount) VALUES ('{vending_id}','{product_id}','{product_amount}')"
+        cur.execute(query_statement)
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/stock')
 
 if __name__ == '__main__':
     app.run(debug=True)
