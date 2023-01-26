7dbc18da16e4fc53b0c2f99918ae384a469f58bf
JackKrubb <jacklee.leeco@gmail.com>
2023-01-26 23:03:32 +0700

    deleted redundant file and created a Connection class for encapsulating sql_connection and provide interfaces for the functions I needed

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_bootstrap import Bootstrap
 from commands.vending_machine_command import Vending_Machine
 from commands.product_command import Product
 from commands.stock_command import Stock
+from sql_connection import Connection
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -21,11 +22,10 @@ mysql = MySQL(app)
 
 @app.route('/vending')
 def all_vending():
-    cur = mysql.connection.cursor()
-    all_vending_machines = cur.execute(Vending_Machine.SELECT_ALL)
+    sql_connection = Connection(mysql)
+    all_vending_machines = sql_connection.execute(Vending_Machine.SELECT_ALL)
     if (all_vending_machines > 0):
-        vend = cur.fetchall()
-        cur.close()
+        vend = sql_connection.fetch_all_data()
         return jsonify(vend)
     else:
         return None
@@ -34,58 +34,53 @@ def all_vending():
 
 @app.route('/vending/single/<int:id>', methods=['GET'])
 def one_vending(id):
-    cur = mysql.connection.cursor()
-    all_vending_machines_by_id = cur.execute(Vending_Machine.get_vending_machine_by_id(id))
+    sql_connection = Connection(mysql)
+    all_vending_machines_by_id = sql_connection.execute(Vending_Machine.get_vending_machine_by_id(id))
     if (all_vending_machines_by_id > 0):
-        vend = cur.fetchone()
-        cur.close()
+        vend = sql_connection.fetch_one_data()
         return jsonify(vend)
     else:
         return None
 
 # Create new vending machine
 
-
 @app.route('/vending/create-vending', methods=['POST'])
 def create_vending():
+    sql_connection = Connection(mysql)
     vending_form = request.form
     vending_location = vending_form['vending_location']
-    cur = mysql.connection.cursor()
-    cur.execute(Vending_Machine.create_vending_machine(vending_location))
-    mysql.connection.commit()
-    cur.close()
+    sql_connection.execute(Vending_Machine.create_vending_machine(vending_location))
+    sql_connection.commit()
     return redirect('/vending')
 
 
 # Edit vending machine
 @app.route('/vending/edit-vending/<int:id>', methods=['POST'])
 def edit_vending(id):
-    cur = mysql.connection.cursor()
+    sql_connection = Connection(mysql)
     vending_form = request.form
     new_vending_location = vending_form['vending_location']
-    cur.execute(Vending_Machine.edit_vending_machine_by_id(new_vending_location, id))
-    mysql.connection.commit()
-    cur.close()
+    sql_connection.execute(Vending_Machine.edit_vending_machine_by_id(new_vending_location, id))
+    sql_connection.commit()
     return redirect('/vending')
 
 # Delete one vending machine
 
 @app.route('/vending/delete-vending/<int:id>')
 def delete_vending(id):
-    cur = mysql.connection.cursor()
-    cur.execute(Vending_Machine.delete_vending_machine_by_id(id))
-    mysql.connection.commit()
+    sql_connection = Connection(mysql)
+    sql_connection.execute(Vending_Machine.delete_vending_machine_by_id(id))
+    sql_connection.commit()
     return redirect('/vending')
 
 # Return all products ordered by price
 
 @app.route('/product')
 def all_product():
-    cur = mysql.connection.cursor()
-    all_products = cur.execute(Product.SELECT_ALL)
+    sql_connection = Connection(mysql)
+    all_products = sql_connection.execute(Product.SELECT_ALL)
     if (all_products > 0):
-        prods = cur.fetchall()
-        cur.close()
+        prods = sql_connection.fetch_all_data()
         return jsonify(prods)
     else:
         return None
@@ -94,11 +89,10 @@ def all_product():
 
 @app.route('/product/single/<int:id>', methods=['GET'])
 def one_product(id):
-    cur = mysql.connection.cursor()
-    product_by_id = cur.execute(Product.get_product_by_id(id))
+    sql_connection = Connection(mysql)
+    product_by_id = sql_connection.execute(Product.get_product_by_id(id))
     if (product_by_id > 0):
-        prods = cur.fetchone()
-        cur.close()
+        prods = sql_connection.fetch_one_data()
         return jsonify(prods)
     else:
         return None
@@ -110,50 +104,45 @@ def add_product():
     product_form = request.form
     product_name = product_form['product_name']
     product_price = product_form['product_price']
-    cur = mysql.connection.cursor()
-    cur.execute(Product.add_product(product_name, product_price))
-    mysql.connection.commit()
-    cur.close()
+    sql_connection = Connection(mysql)
+    sql_connection.execute(Product.add_product(product_name, product_price))
+    sql_connection.commit()
     return redirect('/product')
 
 # Edit product
 
 @app.route('/product/edit-product/<int:id>', methods=['POST'])
 def edit_product(id):
-    cur = mysql.connection.cursor()
+    sql_connection = Connection(mysql)
     product_form = request.form
     new_product_name = product_form['product_name']
     new_product_price = product_form['product_price']
-    cur.execute(Product.edit_product_by_id(id, new_product_name, new_product_price))
-    mysql.connection.commit()
-    cur.close()
+    sql_connection.execute(Product.edit_product_by_id(id, new_product_name, new_product_price))
+    sql_connection.commit()
     return redirect('/product')
 
 # Delete one product
 
 @app.route('/product/delete-product/<int:id>')
 def delete_product(id):
-    cur = mysql.connection.cursor()
-    cur.execute(Product.delete_product_by_id(id))
-    mysql.connection.commit()
+    sql_connection = Connection(mysql)
+    sql_connection.execute(Product.delete_product_by_id(id))
+    sql_connection.commit()
     return redirect('/product')
 
 # Return all stocks in all vending machine
 
-
 @app.route('/stock')
 def all_stock():
-    cur = mysql.connection.cursor()
-    all_stocks = cur.execute(Stock.SELECT_ALL)
+    sql_connection = Connection(mysql)
+    all_stocks = sql_connection.execute(Stock.SELECT_ALL)
     if (all_stocks > 0):
-        stock = cur.fetchall()
-        cur.close()
+        stock = sql_connection.fetch_all_data()
         return jsonify(stock)
     else:
         return None
 
 # Return one stock from one vending
-
 
 @app.route('/stock/single-stock', methods=['GET'])
 def one_stock_one_vend():
@@ -161,28 +150,25 @@ def one_stock_one_vend():
     vending_id = stock_form['vending_id']
     product_id = stock_form['product_id']
 
-    cur = mysql.connection.cursor()
-    one_stock_from_one_vend = cur.execute(Stock.get_one_stock_from_one_vend(product_id, vending_id))
+    sql_connection = Connection(mysql)
+    one_stock_from_one_vend = sql_connection.execute(Stock.get_one_stock_from_one_vend(product_id, vending_id))
     if (one_stock_from_one_vend > 0):
-        stock = cur.fetchone()
-        cur.close()
+        stock = sql_connection.fetch_one_data()
         return jsonify(stock)
     else:
         return None
 
 # Return all stock from one vending
 
-
 @app.route('/stock/single-vend', methods=['GET'])
 def all_stock_one_vend():
     stock_form = request.form
     vending_id = stock_form['vending_id']
 
-    cur = mysql.connection.cursor()
-    all_stocks_one_vend = cur.execute(Stock.get_all_stock_from_one_vend(vending_id))
+    sql_connection = Connection(mysql)
+    all_stocks_one_vend = sql_connection.execute(Stock.get_all_stock_from_one_vend(vending_id))
     if (all_stocks_one_vend > 0):
-        stock = cur.fetchall()
-        cur.close()
+        stock = sql_connection.fetch_all_data()
         return jsonify(stock)
     else:
         return None
@@ -196,46 +182,41 @@ def add_stock():
     product_id = stock_form['product_id']
     product_amount = stock_form['product_amount']
 
-    cur = mysql.connection.cursor()
-    duplicate = cur.execute(Stock.get_one_stock_from_one_vend(product_id, vending_id))
+    sql_connection = Connection(mysql)
+    duplicate = sql_connection.execute(Stock.get_one_stock_from_one_vend(product_id, vending_id))
     if duplicate > 0:
-        stock = cur.fetchone()
+        stock = sql_connection.fetch_one_data_without_close()
         stocking_id = stock['stocking_id']
         new_product_amount = int(stock['product_amount']) + int(product_amount)
-        cur.execute(Stock.edit_stock_by_id(new_product_amount, stocking_id))
-        mysql.connection.commit()
-        cur.close()
+        sql_connection.execute(Stock.edit_stock_by_id(new_product_amount, stocking_id))
+        sql_connection.commit()
         return redirect('/stock')
     else:
-        cur.execute(Stock.add_stock(vending_id, product_id, product_amount))
-        mysql.connection.commit()
-        cur.close()
+        sql_connection.execute(Stock.add_stock(vending_id, product_id, product_amount))
+        sql_connection.commit()
         return redirect('/stock')
 
 # Edit stock amount
 
-
 @app.route('/stock/edit-stock', methods=['POST'])
 def edit_stock():
-    cur = mysql.connection.cursor()
+    sql_connection = Connection(mysql)
     stock_form = request.form
     stocking_id = stock_form['stocking_id']
     new_product_amount = stock_form['product_amount']
-    cur.execute(Stock.edit_stock_by_id(new_product_amount, stocking_id))
-    mysql.connection.commit()
-    cur.close()
+    sql_connection.execute(Stock.edit_stock_by_id(new_product_amount, stocking_id))
+    sql_connection.commit()
     return redirect('/stock')
 
 # Delete stock
 
-
 @app.route('/stock/delete-stock')
 def delete_stock():
-    cur = mysql.connection.cursor()
+    sql_connection = Connection(mysql)
     stock_form = request.form
     stocking_id = stock_form['stocking_id']
-    cur.execute(Stock.delete_stock_by_id(stocking_id))
-    mysql.connection.commit()
+    sql_connection.execute(Stock.delete_stock_by_id(stocking_id))
+    sql_connection.commit()
     return redirect('/stock')
 
 
